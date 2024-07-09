2a1289633a7cebb9e7a6a70a19e7702bee588924
wxj <2574762712@qq.com>
2024-07-09 22:59:58 +0800

    string and function

--- Lesson_02_Vector/Program.cs
+++ Lesson_02_Vector/Program.cs
@@ -14,38 +14,76 @@ class Program
         #endregion
 
         #region Vector' Usage
-        int[] array = { 1, 2, 3, 4, 5 };
-        Console.WriteLine(array.Length);
-        // 遍历数组 for
-        int a = 3;
-        for (int i = 0; i < array.Length; i++)
-        {
-            if (array[i] == a)
-            {
-                Console.WriteLine("索引值的位置在{0}", i);
-                break;
-            }
-        }
+        // int[] array = { 1, 2, 3, 4, 5 };
+        // Console.WriteLine(array.Length);
+        // // 遍历数组 for
+        // int a = 3;
+        // for (int i = 0; i < array.Length; i++)
+        // {
+        //     if (array[i] == a)
+        //     {
+        //         Console.WriteLine("索引值的位置在{0}", i);
+        //         break;
+        //     }
+        // }
         #endregion
 
         Console.WriteLine("----------------------------");
         Console.WriteLine("Vector 2' Usage");
         #region 2. Vector 2
-        int[,] arr;                 // 声明
-        int[,] arr2 = new int[3, 3];
-        int[,] arr3 = new int[3, 3] { {1, 2, 3}, 
-                                      {4, 5, 6}, 
-                                      {7, 8, 9 } };  // 3 * 3
-        int[,] arr4 = new int[,] {  {1, 2, 3}, 
-                                    {4, 5, 6},   
-                                    {7, 8, 9 } }; 
-        int[,] arr5 = { {1, 2, 3}, 
-                        {4, 5, 6}, 
-                        {7, 8, 9} }; 
-        // 得到多少行
-        Console.WriteLine(arr5.GetLength(0));
-        // 得到多少列
-        Console.WriteLine(arr5.GetLength(1));
+        // int[,] arr;                 // 声明
+        // int[,] arr2 = new int[3, 3];
+        // int[,] arr3 = new int[3, 3] { {1, 2, 3},
+        //                               {4, 5, 6},
+        //                               {7, 8, 9 } };  // 3 * 3
+        // int[,] arr4 = new int[,] {  {1, 2, 3},
+        //                             {4, 5, 6},
+        //                             {7, 8, 9 } };
+        // int[,] arr5 = { {1, 2, 3},
+        //                 {4, 5, 6},
+        //                 {7, 8, 9} };
+        // // 得到多少行
+        // Console.WriteLine(arr5.GetLength(0));
+        // // 得到多少列
+        // Console.WriteLine(arr5.GetLength(1));
         #endregion
+
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("交错数据");
+        #region 3. 交错数组
+        int[][] arr1;
+        int[][] arr2 = new int[3][];
+        int[][] arr3 = new int[3][] { new int[] {1, 2, 3},
+                                      new int[] {1, 2},
+                                      new int[] {1} };          // 行数固定，列数不定
+        int[][] arr4 = new int[][]  { new int[] {1, 2, 3},
+                                      new int[] {1, 2},
+                                      new int[] {1} };          // 行数不定，列数不定
+        int[][] arr5 = { new int[] {1, 2, 3},
+                         new int[] {1, 2},
+                         new int[] {1} };
+
+        #region 
+        int[][] array = { new int[] {1, 2, 3},
+                          new int[] {4, 5} };
+        // 行数
+        Console.WriteLine(array.GetLength(0));
+        // 某一行的列数
+        Console.WriteLine(array[0].Length);
+        // 获取交错数据的某个元素
+        Console.WriteLine(array[0][1]);
+        // 遍历交错数组
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array[i].Length; j++)
+            {
+                Console.Write(array[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+        #endregion      
+
+        #endregion
+
     }
 }
---
+++ Lesson_03_string/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lesson_03_string;
+
+class Program
+{
+    static void Print(string text)
+    {
+        Console.WriteLine(text);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("特殊的引用类型String");
+        
+        #region 
+        string str1 = "123";
+        string str2 = str1;     // str2 与 str1 指向同一片内存空间
+        Console.WriteLine("old str2:{0}", str2);
+        str2 = "321";           // str2 取消指向 str1, 并重新分配属于自己的一片内存空间
+        Console.WriteLine(str1);
+        Console.WriteLine("new str2:{0}", str2);
+        #endregion
+
+        Print("1111111111");
+    }
+}
