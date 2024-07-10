0d23b248232b5dfd189bf583fcb6f8ce3b4995b6
wxj <2574762712@qq.com>
2024-07-10 22:08:54 +0800

    add:struct

---
+++ Lesson_05_struct/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Lesson_05_struct
+{
+    class Program
+    {
+        #region struct 结构体
+        struct Student
+        {
+            private string name;
+            private int age;
+            private bool gender;
+            private int number;
+
+            // 构造函数(变量初始化)
+            public Student(string name, int age, bool gender, int number)
+            {
+                this.name = name;
+                this.age = age;
+                this.gender = gender;
+                this.number = number;
+            }
+
+            // 结构体的函数不需要 static 关键字
+            public void Speak()
+            {
+                Console.WriteLine("我的名字是{0}, 我今年{1}岁了。", name, age);
+            }
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Struct' Usage");
+
+            Student stu = new Student("Mike", 18, true, 10001);
+            stu.Speak();
+        }
+    }
+};
+
+
